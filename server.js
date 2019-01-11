8ebd22e69d20e25cb70323f36c22e6c7aeb49f83
Paige Gorry <paigeegorry@gmail.com>
2019-01-11 12:59:04 -0800

    onRequest function, async server response console logging

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var http = require('http');
 
-// http.createServer(function(request, response) {
-//   response.writeHead(200, {"Content-Type": "text/plain" });
-//   response.write('Hello World');
-//   response.end();
-// }).listen(8888);
-
-//alternatively, it could have been written like:
-// var server = http.createServer();
-// server.listen(8888);
-
 function onRequest(request, response) {
+  console.log('request received');
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
+  response.write("Hello World!");
   response.end();
 }
 
 http.createServer(onRequest).listen(8888);
+
+console.log('Server has started');
