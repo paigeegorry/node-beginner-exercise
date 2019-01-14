1537356ac5484b637d7765dcddf03bffe7b47527
Paige Gorry <paigeegorry@gmail.com>
2019-01-13 21:07:12 -0800

    responding request handlers with non-blocking operations

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,23 @@
 var exec = require('child_process').exec;
 
-function start() {
-  console.log('Request handle "start" was called');
-  var content = 'empty';
+function start(response) {
+  console.log('Request handler "start" was called');
 
-  exec('ls -lah', function(error, stdout, stderr) {
-    content = stdout;
-  })
-  return content;
+
+exec('find /', 
+  { timeout: 10000, maxBuffer: 20000*1024 },
+  function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end();
+  });
 }
 
-function upload() {
+function upload(response) {
   console.log('Request handler "upload" was called');
-  return 'Hello Upload';
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write('Hello Upload');
+  response.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -4,16 +4,13 @@ var url = require('url');
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log('request for ' + pathname + ' received');
-    
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    var content = route(handle, pathname);
-    response.write(content);
-    response.end();
+    console.log('Request for ' + pathname + ' received');
+
+    route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(8888);
-  console.log('Server has started.');
+  console.log('Server has started');
 }
 
 exports.start = start;
