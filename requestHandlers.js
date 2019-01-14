d00178b7a160d06d79d62d6631aed3cdc58edb76
Paige Gorry <paigeegorry@gmail.com>
2019-01-13 20:51:22 -0800

    blocking and non-blocking

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,13 @@
+var exec = require('child_process').exec;
+
 function start() {
   console.log('Request handle "start" was called');
-  return 'Hello Start';
+  var content = 'empty';
+
+  exec('ls -lah', function(error, stdout, stderr) {
+    content = stdout;
+  })
+  return content;
 }
 
 function upload() {
