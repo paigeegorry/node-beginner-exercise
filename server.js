e0af0466e5d50af86a6cf6f7c00a3609830cde1a
Paige Gorry <paigeegorry@gmail.com>
2019-01-13 20:36:54 -0800

    server working again

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var http = require('http');
-var url = require('url');
 
-function start(route) {
+function start() {
   function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log('Request for ' + pathname + ' received');
-
-    route(pathname);
-
+    console.log('request received');
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write('Hello World');
     response.end();
